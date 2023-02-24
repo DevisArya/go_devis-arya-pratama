2906ad7aa64e85c82502b4bcc68c328d34531d8c
DevisArya <devisaryapratama@gmail.com>
2023-02-24 23:32:38 +0700

    mengerjakan tugas section 7

--- 7_Time_Complexity_and_Space_Complexity/Praktikum/tugas1.go
+++ 7_Time_Complexity_and_Space_Complexity/Praktikum/tugas1.go
@@ -24,7 +24,12 @@ func primeNumber(number float64) bool {
 
 func main() {
 	var hasil bool
-	hasil = primeNumber(7) 
+
+	hasil = primeNumber(1000000007) // true
+   	// hasil = primeNumber(13)         // true
+   	// hasil = primeNumber(17)         // true
+   	// hasil = primeNumber(20)         // false
+   	// hasil = primeNumber(35)         // false
 
 	if hasil == true {
 		fmt.Println("Bilangan Prima")
--- 7_Time_Complexity_and_Space_Complexity/Praktikum/tugas2.go
+++ 7_Time_Complexity_and_Space_Complexity/Praktikum/tugas2.go
@@ -24,10 +24,10 @@ func pow(x, n int) int {
 }
 
 func main() {
-	fmt.Println(pow(2,3)) 
-	fmt.Println(pow(5,3))  
-	fmt.Println(pow(10,2))
-	fmt.Println(pow(2,5))
-	fmt.Println(pow(7,3))
+	fmt.Println(pow(2, 3))  // 8
+   fmt.Println(pow(5, 3))  // 125
+   fmt.Println(pow(10, 2)) // 100
+   fmt.Println(pow(2, 5))  // 32
+   fmt.Println(pow(7, 3))  // 343
 }
 
